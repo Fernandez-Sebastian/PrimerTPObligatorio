92d02fbbcbb5629eb2fe269e2c848f60e352e860
Andrea Maslucan <morenotandrea@gmail.com>
2025-09-20 12:23:52 -0300

    Agregado de Editorial

--- Biblioteca.cs
+++ Biblioteca.cs
@@ -13,20 +13,20 @@
         public Biblioteca()
         {
             //  cuando se crea una biblioteca, por defecto creo estos 14 libros.
-            libros.Add(new Libro("Harry Potter 1", "Fantasía", new DateTime(1997, 6, 26), "J. K. Rowling"));
-            libros.Add(new Libro("El Quijote", "Novela", new DateTime(1605, 1, 16), "Miguel de Cervantes"));
-            libros.Add(new Libro("Cien Años de Soledad", "Realismo mágico", new DateTime(1967, 5, 30), "Gabriel García Márquez"));
-            libros.Add(new Libro("Rayuela", "Novela", new DateTime(2000, 6, 28), "Julio Cortázar"));
-            libros.Add(new Libro("La Iliada", "Épica", new DateTime(1980, 1, 1), "Homero"));
-            libros.Add(new Libro("La Odisea", "Épica", new DateTime(1930, 1, 1), "Homero"));
-            libros.Add(new Libro("Don Juan Tenorio", "Drama", new DateTime(1844, 1, 1), "José Zorrilla"));
-            libros.Add(new Libro("El Principito", "Fantasía", new DateTime(1943, 4, 6), "Antoine de Saint-Exupéry"));
-            libros.Add(new Libro("Hamlet", "Drama", new DateTime(1600, 1, 1), "William Shakespeare"));
-            libros.Add(new Libro("Macbeth", "Drama", new DateTime(1606, 1, 1), "William Shakespeare"));
-            libros.Add(new Libro("1984", "Distopía", new DateTime(1949, 6, 8), "George Orwell"));
-            libros.Add(new Libro("Fahrenheit 451", "Distopía", new DateTime(1953, 10, 19), "Ray Bradbury"));
-            libros.Add(new Libro("El Hobbit", "Fantasía", new DateTime(1937, 9, 21), "J.R.R. Tolkien"));
-            libros.Add(new Libro("El Señor de los Anillos", "Fantasía", new DateTime(1954, 7, 29), "J.R.R. Tolkien"));
+            libros.Add(new Libro("Harry Potter 1", "Fantasía", new DateTime(1997, 6, 26), "J. K. Rowling", "Planeta"));
+            libros.Add(new Libro("El Quijote", "Novela", new DateTime(1605, 1, 16), "Miguel de Cervantes", "Planeta"));
+            libros.Add(new Libro("Cien Años de Soledad", "Realismo mágico", new DateTime(1967, 5, 30), "Gabriel García Márquez", "Planeta"));
+            libros.Add(new Libro("Rayuela", "Novela", new DateTime(2000, 6, 28), "Julio Cortázar", "Planeta"));
+            libros.Add(new Libro("La Iliada", "Épica", new DateTime(1980, 1, 1), "Homero", "Planeta"));
+            libros.Add(new Libro("La Odisea", "Épica", new DateTime(1930, 1, 1), "Homero", "Planeta"));
+            libros.Add(new Libro("Don Juan Tenorio", "Drama", new DateTime(1844, 1, 1), "José Zorrilla", "Planeta"));
+            libros.Add(new Libro("El Principito", "Fantasía", new DateTime(1943, 4, 6), "Antoine de Saint-Exupéry", "Planeta"));
+            libros.Add(new Libro("Hamlet", "Drama", new DateTime(1600, 1, 1), "William Shakespeare", "Planeta"));
+            libros.Add(new Libro("Macbeth", "Drama", new DateTime(1606, 1, 1), "William Shakespeare", "Planeta"));
+            libros.Add(new Libro("1984", "Distopía", new DateTime(1949, 6, 8), "George Orwell", "Planeta"));
+            libros.Add(new Libro("Fahrenheit 451", "Distopía", new DateTime(1953, 10, 19), "Ray Bradbury", "Planeta"));
+            libros.Add(new Libro("El Hobbit", "Fantasía", new DateTime(1937, 9, 21), "J.R.R. Tolkien", "Planeta"));
+            libros.Add(new Libro("El Señor de los Anillos", "Fantasía", new DateTime(1954, 7, 29), "J.R.R. Tolkien", "Planeta"));
         }
 
         public void AltaLector(string nombre, string dni, string direccion)
@@ -93,14 +93,14 @@
             // El método ToShortDateString transforma una fecha en una cadena y la formatea dependiendo el uso horario configurado en la PC.
             //Encabezado para la tabla de libros
             Console.WriteLine(
-                $"{"Título",-30} | {"Autor",-25} | {"Género",-15} | {"Fecha lanzamiento",-12}");
-            Console.WriteLine(new string('-', 90)); // línea separadora
+                $"{"Título",-30} | {"Autor",-25} | {"Género",-15} | {"Fecha lanzamiento",-17} | {"Editorial",-1}");
+            Console.WriteLine(new string('-', 110)); // línea separadora
 
             //Filas para la tabla de libros
             foreach (Libro libro in libros)
             {
                 Console.WriteLine(
-                    $"{libro.Titulo,-30} | {libro.Autor,-25} | {libro.Genero,-15} | {libro.FechaLanzamiento.ToShortDateString(),-12}");
+                    $"{libro.Titulo,-30} | {libro.Autor,-25} | {libro.Genero,-15} | {libro.FechaLanzamiento.ToShortDateString(),-17} | {libro.Editorial,-1}");
             }
         }
 
